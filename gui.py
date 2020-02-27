880959706d90d48ad82b221400fe6e84224a0315
Alexander You <youa@oregonstate.edu>
2020-02-27 11:27:53 -0800

    GUI complete

--- gui.py
+++ gui.py
@@ -10,14 +10,7 @@ import numpy as np
 import pickle
 from _collections import defaultdict
 import random
-
-class Trunk(object):
-    def __init__(self, o_x, o_y, width, angle):
-        self.o_x = o_x
-        self.o_y = o_y
-        self.width = width
-        self.angle = angle
-
+import time
 
 # https://stackoverflow.com/questions/53420826/overlay-two-pixmaps-with-alpha-value-using-qpainter
 def overlay_pixmap(base, overlay):
@@ -51,7 +44,7 @@ class DataLabeler(QMainWindow):
 
         # Drawing-related stuff here
         self.pixmap = QPixmap()
-        self.orig_pixmap = QPixMap()
+        self.orig_pixmap = QPixmap()
         self.image_label = QLabel()
         main_layout.addWidget(self.image_label)
         self.trunks = []
@@ -73,12 +66,17 @@ class DataLabeler(QMainWindow):
         # button_1 = QPushButton('Load')
         # button_1.clicked.connect(self.update_image)
 
-        button_2 = QPushButton('Clear')
-        button_2.clicked.connect(self.clear_overlay)
+        # button_2 = QPushButton('Clear')
+        # button_2.clicked.connect(self.clear_overlay)
 
         # self.text_input = QLineEdit()
         # options_ui.addWidget(button_1)
-        options_ui.addWidget(button_2)
+        # options_ui.addWidget(button_2)
+
+        self.info_label = QLabel()
+        control_label = QLabel('Press F to move forward, D to move back. S saves and E erases.')
+        options_ui.addWidget(self.info_label)
+        options_ui.addWidget(control_label)
 
         main_layout.addLayout(options_ui)
 
@@ -167,6 +165,8 @@ class DataLabeler(QMainWindow):
         self.image_label.setPixmap(self.pixmap)
         self.image_label.show()
         print('New image {} loaded!'.format(image_name))
+        self.info_label.setText('Image: {}'.format(image_name))
+        self.update_pixmap_with_markers()
 
         self.selections = []
 
@@ -185,10 +185,25 @@ class DataLabeler(QMainWindow):
         if len(self.selections) == 3:
             self.image_label.setMouseTracking(False)
             # Process the clicks, add them to the master data list
-            self.current_data[self.previous[self.current]] = 3
-            print(self.current_data)
+
+
+            base = np.array(self.selections[0])
+            up_end = np.array(self.selections[1])
+            last_clicked = np.array(self.selections[2])
+
+            up_vec = up_end - base
+            perp_vec = np.array([up_vec[1], -up_vec[0]])
+            perp_vec_scale = (last_clicked[0] - up_end[0]) / perp_vec[0]
+            width = np.linalg.norm(perp_vec * perp_vec_scale)
+            angle = np.arctan2(up_vec[1], up_vec[0]) + np.pi / 2
+
+            self.current_data[self.previous[self.current]].append(np.array([base[0] * scale, base[1] * scale,
+                                                             angle, width * scale]))
 
             self.selections = []
+
+            print(self.current_data)
+            self.update_pixmap_with_markers()
 
     def handle_mouse_move(self, event):
         pos = event.pos()
@@ -201,8 +216,6 @@ class DataLabeler(QMainWindow):
             last_x, last_y = self.selections[0]
             last_x /= scale
             last_y /= scale
-
-            last = np.array([last_x, last_y])
 
             self.overlay.fill(QtCore.Qt.transparent)
             self.overlay_painter.drawLine(last_x, last_y, click_x, click_y)
@@ -232,11 +245,46 @@ class DataLabeler(QMainWindow):
             self.overlay_painter.drawLine(last[0], last[1], corner_bottom[0], corner_bottom[1])
             self.overlay_painter.drawLine(corner_top[0], corner_top[1], corner_bottom[0], corner_bottom[1])
 
+
         merged = overlay_pixmap(self.pixmap, self.overlay)
-        self.image_label.setPixmap(merged)
+        if len(self.selections) > 0:  # Race condition prevention
+            self.image_label.setPixmap(merged)
 
     def update_pixmap_with_markers(self):
-        data = self.current_data[self.current]
+        data = self.current_data.get(self.previous[self.current], None)
+        if not data:
+            return
+        scale = self.original_size_x / self.pixmap.width()
+
+        new_pixmap = self.orig_pixmap.copy()
+        painter = QPainter(new_pixmap)
+        pen = QPen(QtCore.Qt.green)
+        pen.setWidth(3)
+        painter.setPen(pen)
+
+        for x, y, angle, width in data:
+
+            x /= scale
+            y /= scale
+            width /= scale
+            angle -= np.pi/2
+
+            c1 = np.array([x, y])
+            up_vec = np.array([np.cos(angle), np.sin(angle)]) * width
+            perp_vec = np.array([-up_vec[1], up_vec[0]])
+            c2 = c1 + up_vec
+            c3 = c2 + perp_vec
+            c4 = c1 + perp_vec
+
+            painter.drawLine(c1[0], c1[1], c2[0], c2[1])
+            painter.drawLine(c2[0], c2[1], c3[0], c3[1])
+            painter.drawLine(c3[0], c3[1], c4[0], c4[1])
+            painter.drawLine(c4[0], c4[1], c1[0], c1[1])
+
+        painter.end()
+        self.pixmap = new_pixmap
+        self.image_label.setPixmap(new_pixmap)
+
         # Take orig pixmap, create overlay with all shapes
         # Set as the displayed pixmap
 
@@ -246,12 +294,17 @@ class DataLabeler(QMainWindow):
         self.overlay.fill(QtCore.Qt.transparent)
         self.selections = []
         self.image_label.setPixmap(self.orig_pixmap)
-        self.current_data[self.current] = []
+        self.pixmap = self.orig_pixmap.copy()
+        self.current_data[self.previous[self.current]] = []
 
     def save(self):
         with open('labelled_data.pickle', 'wb') as fh:
             pickle.dump(self.current_data, fh)
         print('Saved data!')
+
+    def closeEvent(self, event):
+        self.save()
+        event.accept()
 
 
 if __name__ == '__main__':
